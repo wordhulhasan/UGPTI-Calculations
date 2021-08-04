270e57716fed026a7f4a5f58f860ebd5a47eae73
Dilip Mistry <dilipkmistry@gmail.com>
2021-08-04 14:09:23 -0500

    Update

--- step7_opex_by_vrm.py
+++ step7_opex_by_vrm.py
@@ -3,8 +3,8 @@ from janitor import clean_names
 
 
 def step6():
-    df = pd.read_csv('version2.csv', dtype='str')
-    df_2 = pd.read_csv('version2.csv', dtype='str')
+    df = pd.read_csv('../UGPTI-Calculations/Output/step6_opex_by_upt.csv', dtype='str')
+    df_2 = pd.read_csv('../UGPTI-Calculations/Output/step6_opex_by_upt.csv', dtype='str')
     df = clean_names(df)
     df = df.head(5)
     df = df.set_index('ntd_id')
@@ -30,41 +30,41 @@ def step6():
     vp = "VP"
     yr = "YR"
     for index, row in df.iterrows():
-        df = totalOpexCalculationbyTrip(df, df_2, index, ar, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, cb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, cc, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, cr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, dr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, dt, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, fb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, hr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, ip, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, jt, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, lr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, mb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, mg, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, or_, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, pb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, rb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, sr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, tb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, tr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, vp, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, yr, 1)
+        df = totalOpexCalculationByVRM(df, df_2, index, ar, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, cb, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, cc, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, cr, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, dr, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, dt, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, fb, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, hr, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, ip, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, jt, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, lr, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, mb, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, mg, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, or_, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, pb, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, rb, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, sr, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, tb, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, tr, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, vp, 0)
+        df = totalOpexCalculationByVRM(df, df_2,index, yr, 1)
 
-    df.to_csv('version2.csv')
+    df.to_csv('../UGPTI-Calculations/Output/step7_opex_by_vrm.csv')
 
 
-def totalOpexCalculationbyTrip(df, df_2, index, mode, flag):
+def totalOpexCalculationByVRM(df, df_2, index, mode, flag):
     if mode == "IP":
         return df
     tdf = df_2.query("ntd_id== @index")
     valOpex = float(tdf['opex_'+mode.lower()].to_string(index=False).replace(',', '').replace('$', '').replace(' ', ''))
     valVrm = float(tdf['vrm_'+mode.lower()].to_string(index=False).replace(',', '').replace('$', '').replace(' ', ''))
     if valVrm == 0:
-        df.loc[index, 'opex_' + mode.lower() + '_vrm'] = 0
+        df.loc[index, 'opex_' + mode.lower() + '_per_vrm'] = 0
     else:
-        df.loc[index, 'opex_' + mode.lower()+'_vrm'] = "{:.2f}".format(valOpex / valVrm)
+        df.loc[index, 'opex_' + mode.lower()+'_per_vrm'] = "{:.2f}".format(valOpex / valVrm)
     return df
 
 
--- step8_opex_by_vrh.py
+++ step8_opex_by_vrh.py
@@ -3,8 +3,8 @@ from janitor import clean_names
 
 
 def step6():
-    df = pd.read_csv('version2.csv', dtype='str')
-    df_2 = pd.read_csv('version2.csv', dtype='str')
+    df = pd.read_csv('../UGPTI-Calculations/Output/step7_opex_by_vrm.csv', dtype='str')
+    df_2 = pd.read_csv('../UGPTI-Calculations/Output/step7_opex_by_vrm.csv', dtype='str')
     df = clean_names(df)
     df = df.head(5)
     df = df.set_index('ntd_id')
@@ -30,41 +30,41 @@ def step6():
     vp = "VP"
     yr = "YR"
     for index, row in df.iterrows():
-        df = totalOpexCalculationbyTrip(df, df_2, index, ar, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, cb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, cc, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, cr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, dr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, dt, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, fb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, hr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, ip, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, jt, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, lr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, mb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, mg, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, or_, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, pb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, rb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, sr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, tb, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, tr, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, vp, 0)
-        df = totalOpexCalculationbyTrip(df, df_2,index, yr, 1)
+        df = totalOpexCalculationByVRH(df, df_2, index, ar, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, cb, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, cc, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, cr, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, dr, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, dt, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, fb, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, hr, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, ip, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, jt, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, lr, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, mb, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, mg, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, or_, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, pb, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, rb, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, sr, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, tb, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, tr, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, vp, 0)
+        df = totalOpexCalculationByVRH(df, df_2,index, yr, 1)
 
-    df.to_csv('version2.csv')
+    df.to_csv('../UGPTI-Calculations/Output/step8_opex_by_vrh.csv')
 
 
-def totalOpexCalculationbyTrip(df, df_2, index, mode, flag):
+def totalOpexCalculationByVRH(df, df_2, index, mode, flag):
     if mode == "IP":
         return df
     tdf = df_2.query("ntd_id== @index")
     valOpex = float(tdf['opex_'+mode.lower()].to_string(index=False).replace(',', '').replace('$', '').replace(' ', ''))
-    valVrm = float(tdf['vrh_'+mode.lower()].to_string(index=False).replace(',', '').replace('$', '').replace(' ', ''))
-    if valVrm == 0:
-        df.loc[index, 'opex_' + mode.lower() + '_vrh'] = 0
+    valVrh = float(tdf['vrh_'+mode.lower()].to_string(index=False).replace(',', '').replace('$', '').replace(' ', ''))
+    if valVrh == 0:
+        df.loc[index, 'opex_' + mode.lower() + '_per_vrh'] = 0
     else:
-        df.loc[index, 'opex_' + mode.lower()+'_vrh'] = "{:.2f}".format(valOpex / valVrm)
+        df.loc[index, 'opex_' + mode.lower()+'_per_vrh'] = "{:.2f}".format(valOpex / valVrh)
     return df
 
 
