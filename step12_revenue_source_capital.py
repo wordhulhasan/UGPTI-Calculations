fdbf699c83952e1defa3d1e2bd71832f5ecc0ae8
Dilip Mistry <dilipkmistry@gmail.com>
2021-08-06 09:58:00 -0500

    Updated

--- step12_revenue_source_capital.py
+++ step12_revenue_source_capital.py
@@ -2,9 +2,9 @@ import pandas as pd
 from janitor import clean_names
 
 def step12():
-    df = pd.read_csv('Output/step11_revenue_source.csv', dtype ='str')
+    df = pd.read_csv('Output/step11_revenue_source_operations.csv', dtype ='str')
     df = clean_names(df)
-    df = df.head(5)
+#    df = df.head(5)
     df = df.set_index('ntd_id')
     df_rev_source = pd.read_csv('NTD_Files/2019 Revenue Sources.csv', dtype ='str')
     df_rev_source = clean_names(df_rev_source)
@@ -14,7 +14,7 @@ def step12():
         for index2, row2 in tdf.iterrows():
             tdf2 = tdf.query("index==@index2")
             val1 = tdf2['funding_category'].to_string(index=False)
-            val2 = tdf2['total'].to_string(index=False)
+            val2 = tdf2['total'].to_string(index=False).replace(',', '').replace('$', '')
             df.loc[index, val1+"_Capital"] = val2
 
     df.to_csv('Output/step12_revenue_source_capital.csv')
